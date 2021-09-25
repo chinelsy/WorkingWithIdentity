c62e98b1351975e5b09a621ba817521830a4b298
chinenye ozoagudike <chinenyeozoagudike@gmail.com>
2021-09-25 18:50:26 +0100

    ok

---
+++ WorkWithIdentity/Controllers/RoleManagerController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
+
+namespace WorkWithIdentity.Controllers
+{
+    public class RoleManagerController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+        public RoleManagerController(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var roles = await _roleManager.Roles.ToListAsync();
+            return View(roles);
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddRole(string roleName)
+        {
+            if (roleName != null)
+            {
+                await _roleManager.CreateAsync(new IdentityRole(roleName.Trim()));
+            }
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public async Task<IActionResult> DeleteRole(string roleName)
+        {
+            var role = await _roleManager.FindByNameAsync(roleName);
+            if (roleName != null)
+            {
+                var result = await _roleManager.DeleteAsync(role);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return View();
+                }
+            }
+            else
+            
+           ModelState.AddModelError("", "No role found");
+           return RedirectToAction("Index");
+            
+        }
+    }
+}
---
+++ WorkWithIdentity/Data/ContextSeed.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Linq;
+using System.Threading.Tasks;
+using WorkWithIdentity.Models;
+
+namespace WorkWithIdentity.Data
+{
+    public static class ContextSeed
+    {
+        public static async Task SeedRolesAsync(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            //Seed Roles
+            await roleManager.CreateAsync(new IdentityRole(Enums.Role.SuperAdmin.ToString()));
+            await roleManager.CreateAsync(new IdentityRole(Enums.Role.Admin.ToString()));
+            await roleManager.CreateAsync(new IdentityRole(Enums.Role.Moderator.ToString()));
+            await roleManager.CreateAsync(new IdentityRole(Enums.Role.Basic.ToString()));
+
+        }
+        public static async Task SeedSuperAdminAsync(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            //Seed Default User
+            var defaultUser = new ApplicationUser
+            {
+                UserName = "chikki",//U are a SuperAdmin
+                Email = "chikki@gmail.com",
+                FirstName = "Missb",
+                LastName = "dike",
+                EmailConfirmed = true,
+                PhoneNumberConfirmed = true
+            };
+            if (userManager.Users.All(u => u.Id != defaultUser.Id))
+            {
+                var user = await userManager.FindByEmailAsync(defaultUser.Email);
+                if (user == null)
+                {
+                    await userManager.CreateAsync(defaultUser, "Chikki123#");
+                    await userManager.AddToRoleAsync(defaultUser, Enums.Role.Basic.ToString());
+                    await userManager.AddToRoleAsync(defaultUser, Enums.Role.Moderator.ToString());
+                    await userManager.AddToRoleAsync(defaultUser, Enums.Role.Admin.ToString());
+                    await userManager.AddToRoleAsync(defaultUser, Enums.Role.SuperAdmin.ToString());
+                }
+
+            }
+        }
+    }
+}
